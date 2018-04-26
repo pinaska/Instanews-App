40fefd6ce779b97aab68bd2fde24f2c3a4c6eb4d
root <root@LAPTOP-2HVIR50P.localdomain>
2018-04-26 16:44:26 -0700

    JS changes: loeading img, shrinking header, some transition, adding mixins

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 $(document).ready(function(){
-
-
-    //here will be script for instanews app
-      // console.log("Are you ready for some serious stuff?")
+$('#loading-image').hide();
       //selecting section from dropdown list in html
       $('#nyt-feed-selection').on('change', function(){
       var section= $(this).val(); //getting value from the dropdown
+
+        $('header').addClass('header-smaller');
+        //to get header with the added class
+
 
   // requesting AJAX from NYT,; Built by LucyBot. www.lucybot.com
     var url = "https://api.nytimes.com/svc/topstories/v2/"+section+".json";
     url += '?' + $.param({
       'api-key': "7331fe55c27e461991e92be4dea1b870"
     });
+    // .append('<img src="../../assets/images/ajax-loader.gif"/>') //adding loading gif
+    $('#loading-image').show();
     $.ajax({
       url: url,
       method: 'GET',
+
     })
 
     //success
     .done(function(data) {
       // console.log(data)
-      $('.stories-list').empty();//clear the category choice after appending
+      $('.story-list').empty();//clear the category choice after appending
+      $('#loading-image').hide();
 
       var story=0;//entering the first result;
       //there should be max 12 stories, only with pics
@@ -34,16 +39,17 @@ $(document).ready(function(){
             // .append("<div>""<p>" + storyAbs + "</p>")//this get me abstract
             // .append('<img src=' + imageUrl+ '>')// this get me image
 
-           $(".stories-list").append('<li class="story-list-item"><a href="' + storyUrl + '" target="_blank" class="story-item" style="background: url(' + imageUrl + ') no-repeat center/cover"><p class="story-item-text">'+ storyAbs+'</p></a></li>');
+           $(".story-list").append('<li class="story-list-item"><a href="' + storyUrl + '" target="_blank" class="story-item" style="background: url(' + imageUrl + ') no-repeat center/cover"><p class="story-item-text" style="opacity:0">'+ storyAbs+'</p></a></li>');
             story += 1; //adding a story if contains pic
           }
       }
-    })
+    })//end of success
+
 
     //error
     .fail(function(err) {
       throw err;
-    });
+    });//end of error
 
   });// end of '#nyt-feed-selection').on 'change' function
       //copy-pasted from NYT
