a1b3042baef38b544e6ba9ef42114f38fe312ba6
root <root@LAPTOP-2HVIR50P.localdomain>
2018-04-30 21:27:20 -0700

    changes in eslinctrc: make single quote default in JS, style the loading gif: postion absolute plus three lines of code positioning it centrally, clear JS for .smaller-header class: remove the jquery css method from JS file and add the style instead to styles.css

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
 
 
   // requesting AJAX from NYT,; Built by LucyBot. www.lucybot.com
-    var url = "https://api.nytimes.com/svc/topstories/v2/"+section+".json";
+    var url = 'https://api.nytimes.com/svc/topstories/v2/'+section+'.json';
     url += '?' + $.param({
-      'api-key': "7331fe55c27e461991e92be4dea1b870"
+      'api-key': '7331fe55c27e461991e92be4dea1b870'
     });
     $('#loading-image').show();
     $.ajax({
@@ -21,10 +21,9 @@ $(document).ready(function(){
 
     //success
     .done(function(data) {
-      // console.log(data)
       $('.story-list').empty();
       $('#loading-image').hide();
-      $('header').css("margin-top", "0");
+    
 
       var story=0;//entering the first result;
       //there should be max 12 stories, only with pics
@@ -36,7 +35,7 @@ $(document).ready(function(){
             var storyAbs = data.results[i].abstract;
 
 
-           $(".story-list").append('<li class="story-list-item"><a href="' + storyUrl + '" target="_blank" class="story-item" style="background: url(' + imageUrl + ') no-repeat center/cover"><p class="story-item-text">'+ storyAbs+'</p></a></li>');
+           $('.story-list').append('<li class="story-list-item"><a href="' + storyUrl + '" target="_blank" class="story-item" style="background: url(' + imageUrl + ') no-repeat center/cover"><p class="story-item-text">'+ storyAbs+'</p></a></li>');
             story += 1;
           }
       }
@@ -44,8 +43,8 @@ $(document).ready(function(){
 
 
     //error
-    .fail(function(err) {
-      throw err;
+    .fail(function() {
+      $('.story-list').append('<p>Sorry, it appears there is a problem. Try agai later</p>');
     });//end of error
 
   });// end of $('#nyt-feed-selection').on 'change'function
